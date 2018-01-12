dcef6891f1b09d952894b78b86599695c557f5b1
STOQE <stoqe@stoqe.com>
2018-01-12 15:34:18 +1100

    + Map locator (basic)

--- js/search-simplified.js
+++ js/search-simplified.js
@@ -181,3 +181,82 @@ function getTemplate(templateName) {
 function getHeader(title) {
   return `<h5>${title}</h5>`;
 }
+
+
+
+var apts = instantsearch({
+  appId: 'AQM12ZJFZT',
+  apiKey: 'c33a6b6f6c0912ae9cd61765e90e2dd2',
+  indexName: 'locations_dev'
+});
+
+var aptsHits = instantsearch.widgets.hits({
+  container: document.querySelector('#hits'),
+  hitsPerPage: 10,
+  templates: {
+    item: '<strong>{{{_highlightResult.SiteName.value}}}</strong> — {{{_highlightResult.ShopLevel.value}}} {{{_highlightResult.Building.value}}} {{{_highlightResult.StreetNumber.value}}} {{{_highlightResult.StreetAddress.value}}}, {{{_highlightResult.Suburb.value}}} {{{_highlightResult.Postcode.value}}}',
+    empty: getTemplate('no-results')
+  }
+});
+
+var searchBox = instantsearch.widgets.searchBox({
+  container: document.querySelector('#searchBox'),
+  wrapInput: false
+});
+
+var customMapWidget = {
+  _autocompleteContainer: document.querySelector('#places'),
+  _mapContainer: document.querySelector('#map'),
+  _hitToMarker: function(hit) {
+    return new google.maps.Marker({
+      position: {lat: hit._geoloc.lat, lng: hit._geoloc.lng},
+      map: this._map,
+      title: hit.name
+    });
+  },
+  _handlePlaceChange: function(place) {
+    // https://developers.google.com/maps/documentation/javascript/reference#Autocomplete
+    var place = this._autocomplete.getPlace();
+
+    if (place.geometry === undefined) {
+      // user did not select any place, see https://developers.google.com/maps/documentation/javascript/reference#Autocomplete
+      // events paragraph
+      if (place.name === '') {
+        // input was cleared
+        this._helper
+          .setQueryParameter('aroundLatLng')
+          .search();
+      }
+      return;
+    }
+
+    // see https://developers.google.com/maps/documentation/javascript/reference#PlaceResult
+    var latlng = place.geometry.location.toUrlValue();
+
+    // https://www.algolia.com/doc/guides/geo-search/geo-search-overview/#filter-and-sort-around-a-location
+    this._helper
+      .setQueryParameter('aroundLatLng', latlng)
+      .search();
+  },
+  init: function(params) {
+    this._helper = params.helper;
+    this._autocomplete = new google.maps.places.Autocomplete(
+        (this._autocompleteContainer),
+        {types: ['geocode'], componentRestrictions: {country: 'au'}});
+    this._autocomplete.addListener('place_changed', this._handlePlaceChange.bind(this));
+    this._map = new google.maps.Map(this._mapContainer, {zoom: 1, center: new google.maps.LatLng(0, 0)});
+  },
+  render: function(params) {
+    var markers = params.results.hits.map(this._hitToMarker.bind(this));
+    var bounds = new google.maps.LatLngBounds();
+    markers.forEach(function(marker) {
+      bounds.extend(marker.getPosition());
+    });
+    this._map.fitBounds(bounds);
+  }
+};
+
+apts.addWidget(searchBox);
+apts.addWidget(aptsHits);
+apts.addWidget(customMapWidget);
+apts.start();
