9fb900828ff6890a3ec29c4b6425b683d113805a
STOQE <stoqe@stoqe.com>
2018-04-11 08:25:16 +1000

    ~ Aloglia API key change

--- static/js/search-simplified.js
+++ static/js/search-simplified.js
@@ -2,7 +2,8 @@
 
 app({
   appId: 'AQM12ZJFZT',
-  apiKey: 'c33a6b6f6c0912ae9cd61765e90e2dd2',
+  //apiKey: 'c33a6b6f6c0912ae9cd61765e90e2dd2',
+  apiKey: '2cf4da41c1f1fc39b38eec650ca53dd0',
   indexName: 'locations',
 });
 
